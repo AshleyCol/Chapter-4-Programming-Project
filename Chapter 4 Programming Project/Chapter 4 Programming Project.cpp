dc11d711d2ae7b3ef393dcad15446c7860825440
AshleyCol <ashe.coleman1022@gmail.com>
2026-05-01 23:57:32 -0400

    completed

--- Chapter 4 Programming Project/Chapter 4 Programming Project.cpp
+++ Chapter 4 Programming Project/Chapter 4 Programming Project.cpp
@@ -4,30 +4,31 @@ using namespace std;
 
 int main()
 {
-    int month = 0;
-    int year = 0;
-    
-    cout << "Please enter a month(1-12): ";
-    cin >> month;
+    float weight = 20;
+    int distance = 0;
 
-    cout << "\nPlease enter a year: ";
-    cin >> year;
-    
-    if (month == 2)
-        if (year % 4 == 0)
-            if (year % 100 == 0)
-                if (year % 400 == 0)
-                    cout << "29";
-                else
-                    cout << "28";
-            else
-                cout << "29";
-        else
-            cout << "28";
-    else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-        cout << "31";
-    else if (month == 4 || month == 6 || month == 9 || month == 11)
-        cout << "30";
+    cout << "How much does the package weigh? (kg)\n";
+    cin >> weight;
+    if (weight <= 0 or weight > 20) {
+        cout << "weight is not within restrictions";
+        return 0;
+    }
+    cout << "How far are you shipping it? (miles)\n";
+    cin >> distance;
+    if (distance < 20 or distance > 3000) {
+        cout << "distance is not within restrictions";
+        return 0;
+    }
+    if (weight <= 2)
+        cout << "the price is $" << 1.10 * (distance / 500);
     else
-        cout << "invalid input! please enter a number between 1 and 12";
+        if (weight < 6)
+            cout << "the price is $" << 2.20 * (distance / 500);
+        else
+            if (weight < 10)
+                cout << "the price is $" << 3.70 * (distance / 500);
+            else
+                cout << "the price is $" << 4.80 * (distance / 500);
+
+    return 0;
 }
